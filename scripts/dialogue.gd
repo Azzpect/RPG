f2b98c7719fbb2984d8dd516703419dabfa4dac6
Azzpect <atanughosh.brp@gmail.com>
2024-09-21 13:22:25 +0530

    fixed screen resolution and added the npc face in dialogue

--- scripts/dialogue.gd
+++ scripts/dialogue.gd
@@ -2,15 +2,18 @@ extends Node2D
 
 
 signal _initializeDialogueSystem
-signal _showDialogue
+signal _showDialogue(name: String, dialogue: String)
 signal _hideDialogueBox
 
 @onready var animationPlayer: AnimationPlayer = $AnimationPlayer
 @onready var label: Label = $MarginContainer/Label
 @onready var timer: Timer = $Timer
 @onready var gameManager: Node = %game_manager
+@onready var face: Sprite2D = $npcFace
 
 var dialogueRunning = false
+
+var face_cache = {}
 
 func _ready():
 	connect("_initializeDialogueSystem", initializeDialogueSystem)
@@ -26,13 +29,21 @@ func hideDialogueBox():
 	dialogueRunning = false
 	animationPlayer.play("dialogue_box_disappear")
 
-func showDialogue(dialogue: String):
+func showDialogue(name: String, dialogue: String):
+	var face_texture = null
+	if face_cache.has(name):
+		face_texture = face_cache[name]
+	else:
+		var face_loc: String = "res://Assets/Face/" + name + ".png"
+		face_texture = load(face_loc)
+		face_cache[name] = face_texture
+	face.texture = face_texture
 	printLetters(dialogue)
 
 func printLetters(dialogue: String):
 	label.text = ""
 	animationPlayer.stop()
-	$Polygon2D.visible = false
+	$MarginContainer/Polygon2D.visible = false
 	if !dialogueRunning:
 		return
 	for ch in dialogue:
--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -16,21 +16,23 @@ var dialogues = [
 	# "As long as I don't give up I believe they will lend me their power",
 	# "Why don't you wake up now?",
 	# "It's almost 10'o clock, Johny, Johny, wake up. The breakfast is ready, wake up quick."
-	"You foolish Knight, how could you even think of defeating me.",
-	"You are Nothing but a mere mortal who wanted to defy the one true King",
-	"With your Fall,this world of yours who put all their worthless hope in your being will fall too.",
-	"Consumed in darkness and slowly being devoured by this almighty.","I..I wont let that happen",
-	"Ha!You won't let that happen?",
-	"You! Who is fallen on his knees",
-	"Who is barely alive because of the mercy of this True King",
-	"Its true that I am on my knees","But I still have not fallen",
-	"And I will Fight until every fibre of my being seize to exist",
-	"For I carry the Hopes of humanity",
-	"Thats Commendeble!",
-	"But why don't you wake now?",
-	"It's almost 10'o clock",
-	"You better wake up Sid,and come have your breakfast.",
-	"Huh! wha...what are you saying?"
+	{"name": "Demon Lord", "dialogue": "You foolish Knight, how could you even think of defeating me."},
+	{"name": "Demon Lord", "dialogue": "You are nothing but a mere mortal who wanted to defy the one true King."},
+	{"name": "Demon Lord", "dialogue": "With your fall, this world of yours who put all their worthless hope in your being will fall too."},
+	{"name": "Demon Lord", "dialogue": "Consumed in darkness and slowly being devoured by this almighty."},
+	{"name": "Knight", "dialogue": "I..I won't let that happen."},
+	{"name": "Demon Lord", "dialogue": "Ha! You won't let that happen?"},
+	{"name": "Demon Lord", "dialogue": "You! Who has fallen on his knees."},
+	{"name": "Demon Lord", "dialogue": "Who is barely alive because of the mercy of this True King."},
+	{"name": "Knight", "dialogue": "It's true that I am on my knees."},
+	{"name": "Knight", "dialogue": "But I still have not fallen."},
+	{"name": "Knight", "dialogue": "And I will fight until every fiber of my being ceases to exist."},
+	{"name": "Knight", "dialogue": "For I carry the hopes of humanity."},
+	{"name": "Demon Lord", "dialogue": "That's commendable!"},
+	{"name": "Demon Lord", "dialogue": "But why don't you wake now?"},
+	{"name": "Demon Lord", "dialogue": "It's almost 10 o'clock."},
+	{"name": "Demon Lord", "dialogue": "You better wake up Sid, and come have your breakfast."},
+	{"name": "Knight", "dialogue": "Huh! What are you saying?"}
 ]
 var i = 0
 var isSpaceClicked = false
@@ -42,7 +44,7 @@ func _ready():
 	timer.start()
 	await timer.timeout
 	dialogueBox.emit_signal("_initializeDialogueSystem")
-	dialogueBox.emit_signal("_showDialogue", dialogues[i])
+	dialogueBox.emit_signal("_showDialogue", dialogues[i]["name"], dialogues[i]["dialogue"])
 	i += 1
 
 
@@ -62,7 +64,7 @@ func lineCompleted():
 		i = 0
 		dialogueBox.emit_signal("_hideDialogueBox")
 		return
-	dialogueBox.emit_signal("_showDialogue", dialogues[i])
+	dialogueBox.emit_signal("_showDialogue", dialogues[i]["name"], dialogues[i]["dialogue"])
 	isLineFinished = false
 	isSpaceClicked = false
 	i += 1
