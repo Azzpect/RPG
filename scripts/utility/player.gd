da793e09b708424327567899165ca607d3045f2d
Azzpect <atanughosh.brp@gmail.com>
2024-09-28 15:38:41 +0530

    completed player animations and player state machine

--- scripts/utility/player.gd
+++ scripts/utility/player.gd
@@ -2,7 +2,8 @@ extends CharacterBody2D
 
 
 var movement: Vector2
-var speed: int = 100
+var last_direction: Vector2 = Vector2.ZERO
+var speed: int = 50
 
 @onready var animation_tree: AnimationTree = $AnimationTree
 
@@ -12,9 +13,17 @@ func _ready() -> void:
 
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta: float) -> void:
-	movement = Input.get_vector("left", "right", "up", "down").normalized() * speed
-	animation_tree.set("parameters/blend_position", movement)
-	velocity = movement
+	if movement[1] == 0:
+		movement[0] = Input.get_axis("left", "right")
+	if movement[0] == 0:
+		movement[1] = Input.get_axis("up", "down")
+	
+	if movement != Vector2.ZERO:
+		last_direction = movement
+	
+	animation_tree.set("parameters/playerStates/playerIdle/blend_position", last_direction)
+	animation_tree.set("parameters/playerStates/playerWalk/blend_position", last_direction)
+	velocity = movement.normalized() * speed
 	move_and_slide()
