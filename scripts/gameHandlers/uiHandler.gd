4fbd2b5d4b62b3afbf98d810a2fbf6a59f0f6fbe
Azzpect <atanughosh.brp@gmail.com>
2024-10-16 20:14:05 +0530

    added the quest icon and on press functionality

--- scripts/gameHandlers/uiHandler.gd
+++ scripts/gameHandlers/uiHandler.gd
@@ -1,5 +1,10 @@
 extends Control
 
+@onready var questBox: NinePatchRect = %questBox
 
 func _on_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_quest_pressed() -> void:
+	questBox.visible = !questBox.visible
