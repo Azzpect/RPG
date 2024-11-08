ab58ad33a22458a1a54fd3f576b583e30fb49d89
Azzpect <atanughosh.brp@gmail.com>
2024-11-08 20:48:24 +0530

    changed the dialogue

--- dll/game.gd
+++ dll/game.gd
@@ -6,5 +6,9 @@
         Sid: This is my first time playing dad's game
         Sid: I don't know what to expect
         /set background texture load(res://Assets/cutscene/gameScene2.png)
+        Sid: Actually not bad.
+        /set background texture load(res://Assets/cutscene/gameScene3.png)
+        Sid: But what is happening?
+        Sid: Aaaahaaah!
     "
 }
